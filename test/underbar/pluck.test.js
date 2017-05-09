7f770a6e2fb86b313f52b8d50e1178ef785b5f4e
azmeer <azmeer.salleh@gmail.com>
2017-05-09 10:29:55 -0700

    Adding unit test for pluck

--- test/underbar/pluck.test.js
+++ test/underbar/pluck.test.js
@@ -2,7 +2,12 @@ const _ = require('../../underbar');
 
 describe('pluck()', () => {
   it('returns an array of just ages, given an array of people objects', () => {
-    // Your code goes here
+    const people = [
+      {name: 'Harry', age: 16},
+      {name: 'Hermione', age: 15},
+      {name: 'Ron', age: 12}
+    ];
+    expect(_.pluck(people, 'age')).toEqual([16, 15, 12]);
   });
 
 });
